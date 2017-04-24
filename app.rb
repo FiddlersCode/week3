4f7f603c84efc4a2565724c033c1239ebadbe0b6
milesillsley <milesillsley@hotmail.co.uk>
2017-04-24 13:30:06 +0100

    Adding cat

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+#set :session_secret, 'super secret'
 
 get '/' do
   'hello!'
+end
+
+get '/secret' do
+  'not a secret!'
+end
+
+get '/the real secret' do
+  'n/abcdef'
+end
+
+get '/cat' do
+  <div>
+    <img src='http://bit.ly/1eze8aE'
+  </div>
 end
